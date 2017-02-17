06875b8cdb1305c71eb550e57210104ed24be0c5
weblabs7 <weblabs@live.com>
2017-02-17 15:40:30 +0000

    added to pics view new and form via haml.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+    resources :pics
+    root 'pics#index'
+    
+    
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
