3394339e4d64d679fbbda9254d7c5657985cef5b
weblabs7 <weblabs@live.com>
2017-02-17 17:39:23 +0000

    show an index page.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,10 @@
 class PicsController < ApplicationController
+    before_action :find_pic, only:[:show, :edit, :update, :destroy]
     
     def index
+    end
+    
+    def show
     end
     
     def new
@@ -9,11 +13,21 @@ class PicsController < ApplicationController
     
     def create
         @create = Pic.new(pic_params)
+        
+        if @pic.save
+            redirect_to @pic, notice:"Picture was saved successfully."
+        else 
+            render 'new'
+        end
     end
     
     private 
     
     def pic_params
         params.require.(:pic).permit(:title, :description)
+    end
+    
+    def find_pic
+        @pic = Pic.find(params[:id])
     end
 end
